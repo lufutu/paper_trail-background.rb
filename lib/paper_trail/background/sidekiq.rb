45d94af7a09b209c4c804011d48b842c9d851c65
Tu Truong <tu.truong@med247.co>
2019-11-27 14:53:25 +0700

    Update sidekiq.rb
    Create version if exist attributes

--- lib/paper_trail/background/sidekiq.rb
+++ lib/paper_trail/background/sidekiq.rb
@@ -3,8 +3,8 @@ module PaperTrail
     module Sidekiq
       def perform(version_class, attributes, event)
         puts "VERSION ATTRIBUTES"
-        puts JSON.parse(attributes).inspect
-        version = version_class.constantize.create!(attributes.instance_of?(String) ? JSON.parse(attributes) : attributes)
+        puts attributes.inspect
+        version = version_class.constantize.create!(JSON.parse(attributes)) if attributes.present?
       end
     end
   end
