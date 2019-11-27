37055bd1017e8f721d10da5f5f9e59ef8d4706a8
Tu Truong <tu.truong@med247.co>
2019-11-27 12:31:55 +0700

    Update background.rb
    Change version class to string

--- lib/paper_trail/background.rb
+++ lib/paper_trail/background.rb
@@ -73,7 +73,7 @@ module PaperTrail
     end
 
     private def trigger_write(record, data, event)
-      version_class = record.class.paper_trail.version_class
+      version_class = record.class.paper_trail.version_class.to_s
 
       # ActiveRecord::Base.after_transaction do
         VersionJob.perform_later(
