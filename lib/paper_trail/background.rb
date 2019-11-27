77cbe65af6db3587d0944ca6d7467b3e6c88ab4c
Tu Truong <tu.truong@med247.co>
2019-11-27 12:24:15 +0700

    Update background.rb
    Try change after_transction not class

--- lib/paper_trail/background.rb
+++ lib/paper_trail/background.rb
@@ -75,7 +75,7 @@ module PaperTrail
     private def trigger_write(record, data, event)
       version_class = record.class.paper_trail.version_class
 
-      version_class.after_transaction do
+      ActiveRecord::Base.after_transaction do
         VersionJob.perform_async(
           version_class,
           data.merge(
