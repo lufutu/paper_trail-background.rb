199fbb649a1ac19020ed2adbe70d881630375113
Tu Truong <tu.truong@med247.co>
2019-11-27 14:03:48 +0700

    Update sidekiq.rb
    Parse if String version create new

--- lib/paper_trail/background/sidekiq.rb
+++ lib/paper_trail/background/sidekiq.rb
@@ -2,7 +2,7 @@ module PaperTrail
   module Background
     module Sidekiq
       def perform(version_class, attributes, event)
-        version = version_class.constantize.create!(attributes)
+        version = version_class.constantize.create!(attributes.instance_of?(String) ? JSON.parse(attributes) : attributes)
       end
     end
   end
